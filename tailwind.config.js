475cee1d52af0ed6fe74ae784ecd95c13f5a1f32
Boris zaidan <Bzmgs31@gmail.com>
2025-02-01 13:41:59 -0300

    chore: atualiza Taiwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./index.html", // Se você tiver esse arquivo, inclua
+    "./src/**/*.{js,jsx,ts,tsx}", // Arquivos dentro da pasta src (React)
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: "#0F0E17", // Cor personalizada
+        textColor: "#FFFFFE",
+        buttonColor: "#FF8906",
+      },
+    },
   },
   plugins: [],
 };
