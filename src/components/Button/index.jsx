eb48bce965e388fb7a0add40b4f8776ad12d2814
Boris zaidan <Bzmgs31@gmail.com>
2025-02-06 22:44:14 -0300

    feat(button): tornar o botão reutilizável com suporte a propriedades dinâmicas

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
-const Button = () => {
-  return <div></div>;
+const Button = ({ children, className, onClick, type = "button" }) => {
+  return (
+    <button
+      type={type}
+      onClick={onClick}
+      className={`bg-buttonColor h-14 px-4 py-2 rounded-lg text-textColor font-semibold w-8/12 max-w-xs shadow-lg hover:bg-buttonColor/80 ${className}`}
+    >
+      {children}
+    </button>
+  );
 };
 
 export default Button;
