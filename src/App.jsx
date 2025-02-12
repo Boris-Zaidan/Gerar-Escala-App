37374461623ce7844407af3615c6496a46ca6a7f
Boris zaidan <Bzmgs31@gmail.com>
2025-02-12 13:23:27 -0300

    feat(home): adicionar seção de ministério com estilos responsivos

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import AppRoutes from "./routes/Routes";
 
 const App = () => {
   return (
-    <div className=" w-screen h-screen flex justify-center items-center">
+    <div className=" w-screen h-screen flex justify-center items-center m-0">
       <AppRoutes />
     </div>
   );
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,30 +3,39 @@ import Button from "../../components/Button";
 
 const Home = () => {
   return (
-    <main className="flex flex-col w-screen h-screen items-center justify-between p-2">
-      <section className="w-full flex justify-center">
+    <main
+      className="flex flex-col w-full h-dvh overflow-hidden items-center justify-between p-2  sm:w-full sm:h-dvh
+      md:max-w-full md:h-screen md:flex md:flex-col md:justify-center md:items-center "
+    >
+      <section className="w-full h-3/6  flex justify-center sm:flex sm:flex-col sm:justify-center sm:items-center sm:w-full sm:h-96 md:w-full md:h-2/4 md:m-0 md:justify-center md:items-center lg:max-w-5xl">
         <img
           src={img}
           alt="Logotipo da igreja IECO"
-          className="w-full max-w-md object-cover rounded-md shadow-md"
+          className="w-full h-full object-cover rounded-md shadow-md  sm:w-5/6  sm:h-5/6"
         />
       </section>
 
-      <section className="w-full flex flex-col items-center text-center mt-1">
-        <h2 className="text-3xl md:text-3xl font-bold text-textColor">
+      <section className="w-full  h-2/5  pt-3 flex flex-col items-center text-center  mt-1 md:w-full  md:mt-5 ">
+        <h2 className="text-3xl md:text-5xl font-bold text-textColor  md:font-semibold">
           Ministério
         </h2>
-        <ul className="flex gap-5 mt-6 text-textColor">
-          <li className="text-base font-medium">Pr. Oséias Fernandes</li>
-          <li className="text-base font-medium">Pra. Marcileia Fernandes</li>
+        <ul className="flex gap-5 mt-6 text-textColor md:gap-10">
+          <li className="text-base font-medium  md:text-3xl md:font-normal">
+            Pr. Oséias Fernandes
+          </li>
+          <li className="text-base font-medium md:text-3xl md:font-normal">
+            Pra. Marcileia Fernandes
+          </li>
         </ul>
-        <span className="text-textColor/95 mt-6">
+        <span className="text-textColor/95 mt-6 md:text-2xl md:font-light">
           Equipe de Pastores e Obreiros
         </span>
       </section>
 
-      <div className="w-full flex justify-end mr-3 mt-6">
-        <Button>Avançar</Button>
+      <div className="w-full  h-1/6 flex justify-end mr-3 mt-3  sm:w-5/6 md:mb-3  md:w-full md:h-1/4 md:flex md:items-end md:justify-end lg:max-w-5xl lg:flex lg:justify-end lg:p-0">
+        <Button className="hover:bg-buttonColor/75  py-2.5 px-4">
+          Avançar
+        </Button>
       </div>
     </main>
   );
