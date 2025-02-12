72cd474a86fe48d924468bc7cedd22ffa75cad84
Boris zaidan <Bzmgs31@gmail.com>
2025-02-12 14:21:39 -0300

    feat(routes): adicionar rota de boas-vindas no React Router

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
+import Welcome from "../pages/Welcome";
 
 function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/Welcome" element={<Welcome />} />
       </Routes>
     </BrowserRouter>
   );
