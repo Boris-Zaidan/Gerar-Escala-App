34e76fcd64fbd1bfebbddfb5fca8f56df09c7a99
Boris zaidan <Bzmgs31@gmail.com>
2025-02-13 12:14:42 -0300

    feat(routes): Adicionar rota de login

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
 import Welcome from "../pages/Welcome";
+import Login from "../pages/Login";
 
 function AppRoutes() {
   return (
@@ -8,6 +9,7 @@ function AppRoutes() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Welcome" element={<Welcome />} />
+        <Route path="/Login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
